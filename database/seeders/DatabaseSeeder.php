0ef6e1fbf2ce3f5dfc31a9c229cb56727e09f141
Dimas Muhamad Rofiq <dimasmrofiq@students.amikom.ac.id>
2026-05-08 20:22:44 +0700

    revisi

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,8 @@ namespace Database\Seeders;
 
 // Import semua model yang digunakan di sini
 use App\Models\User;
-use App\Models\category;
-use App\Models\event;
+use App\Models\Category;
+use App\Models\Event;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
