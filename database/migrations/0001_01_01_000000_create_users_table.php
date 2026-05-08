2f860e35bc3a8d12cd56fc5f18b6c21b7ba23d91
Dimas Muhamad Rofiq <dimasmrofiq@students.amikom.ac.id>
2026-05-08 11:21:28 +0700

    Menambahkan kolom role di tabel users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             
-            // ---> INI KOLOM BARU YANG KITA TAMBAHKAN <---
+            // Kolom role ditambahkan di sini
             $table->string('role')->default('user');
             
             $table->rememberToken();
